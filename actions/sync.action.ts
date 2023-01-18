862ccc1b5e6ea5a327eeb8024cb69458eeb95ce9
parsasi <mail@parsa.pro>
2023-01-17 20:02:58 -0800

    Custom `BaseException` children handle error `substitutions`

--- actions/sync.action.ts
+++ actions/sync.action.ts
@@ -5,19 +5,20 @@ import { runFetcher } from '../lib/run-fetcher';
 import { createDictionaryFiles } from '../lib/utils/file-system/create-dictionary-files';
 import { AbstractAction } from './abstract.action';
 import { BaseException } from '../exceptions/base.exception';
-import { MESSAGES } from '../lib/ui/messages'
-import { MissingParamException } from '../exceptions/missing-param.exception'
+import { MESSAGES } from '../lib/ui/messages';
+import { MissingParamException } from '../exceptions/missing-param.exception';
 import type { RcFile } from '../interfaces/rc.interface';
 
 export class SyncAction extends AbstractAction {
   public async handle({ options }) {
     try {
-      const { fetcher, resolve, fetcherParams, envPath } = await this.resolveOptions(options);
-      this.validateParams({ fetcher, resolve, fetcherParams, envPath })
-      const output = await runFetcher(fetcher, fetcherParams, envPath); 
-      await createDictionaryFiles(resolve, output) && console.log(chalk.green(MESSAGES.SYNC_SUCCESSFUL));
+      const { fetcher, resolve, fetcherParams, envPath } =
+        await this.resolveOptions(options);
+      this.validateParams({ fetcher, resolve, fetcherParams, envPath });
+      const output = await runFetcher(fetcher, fetcherParams, envPath);
+      await createDictionaryFiles(resolve, output);
+      console.log(chalk.green(MESSAGES.SYNC_SUCCESSFUL));
     } catch (e) {
-      console.log('cathcing in handle')
       if (e instanceof BaseException) {
         console.error(chalk.red(e.getMessage()));
       } else {
@@ -37,23 +38,22 @@ export class SyncAction extends AbstractAction {
     };
   }
 
-  async validateParams(options){
-    const requiredParams = ['fetcher' , 'resolve'];
-    const parameterErrors = requiredParams.reduce((paramErrors , currentParam) => {
-      if(!options[currentParam]){
-        if(paramErrors){
-          return paramErrors += `, ${currentParam}`
-        }else{
-          return currentParam
+  validateParams(options) {
+    const requiredParams = ['fetcher', 'resolve'];
+    const parameterErrors = requiredParams.reduce(
+      (paramErrors, currentParam) => {
+        if (!options[currentParam]) {
+          if (paramErrors) {
+            return (paramErrors += `, ${currentParam}`);
+          } else {
+            return currentParam;
+          }
         }
-      }
-    } , '')
-    try{
-      if(parameterErrors){
-        throw new MissingParamException({ parameters : parameterErrors})
-      }
-    }catch(e){
-      throw e
+      },
+      '',
+    );
+    if (parameterErrors) {
+      throw new MissingParamException({ parameters: parameterErrors });
     }
   }
 
--- exceptions/missing-param.exception.ts
+++ exceptions/missing-param.exception.ts
@@ -2,5 +2,4 @@ import { BaseException } from './base.exception';
 
 export class MissingParamException extends BaseException {
   public MESSAGE_TITLE = 'MISSING_PARAMETER' as const;
-  
 }
