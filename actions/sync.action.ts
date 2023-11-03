33097b54ee0e350f9df427abe9f38362f98e6106
parsasi <mail@parsa.pro>
2023-11-03 08:28:18 -0700

    added connection to mongodb aka `context`

--- actions/sync.action.ts
+++ actions/sync.action.ts
@@ -8,20 +8,27 @@ import { authorWikipediaModifier } from '../lib/modifiers/author-wikipedia-modif
 import { applyModifiers } from '../lib/modifiers';
 import { createTagsHash } from '../lib/utils/create-tags-hash/create-tags-hash';
 import { createBookEntities } from '../lib/utils/create-entities/create-entities';
+import { getOrCreateBook, reportError, updateBook } from '../context/context';
 export class SyncAction extends AbstractAction {
   public async handle({ options }) {
+    const id = options.book ?? '13';
+
+    const contextBook = await getOrCreateBook({
+      id,
+      status: 'pending',
+    });
     try {
-      console.log(chalk.green(MESSAGES.SYNC_SUCCESSFUL));
-
-      const id = options.book ?? '13';
-
       const parsedStuff = await parseRdf(
         join(__dirname, '..', '..', 'data', 'rdf', id, `pg${id}.rdf`),
       );
 
+      updateBook(contextBook._id, parsedStuff);
+
       const modifiedStuff = await applyModifiers([authorWikipediaModifier])(
         parsedStuff,
       );
+
+      updateBook(contextBook._id, modifiedStuff);
 
       const tagsHash = await createTagsHash(
         join(__dirname, '..', '..', 'data', 'tag-hashmap.json'),
@@ -30,9 +37,11 @@ export class SyncAction extends AbstractAction {
       const createdStuff = await createBookEntities(modifiedStuff, tagsHash);
 
       console.log(createdStuff);
+      console.log(chalk.green(MESSAGES.SYNC_SUCCESSFUL));
     } catch (e) {
       if (e instanceof BaseException) {
         //TODO: Report error more elegantly
+        await reportError(contextBook._id, e.getMessage());
         console.error(chalk.red(e.getMessage()));
       } else {
         throw e;
---
+++ context/context.ts
@@ -0,0 +1,154 @@
+import { ParsedBook } from '@lib/utils/parse-rdf/parse-rdf';
+import { MongoClient, Collection, ObjectId } from 'mongodb';
+
+interface Book extends Partial<Omit<ParsedBook, 'authors' | 'tags'>> {
+  _id?: ObjectId;
+  errors?: string[];
+  status: 'pending' | 'completed' | 'failed';
+  authors?: Author[];
+  tags?: Tag[];
+}
+
+interface Tag {
+  _id?: ObjectId;
+  name: string;
+}
+
+interface Author {
+  _id?: ObjectId;
+  name: string;
+}
+
+interface Context {
+  books: Collection<Book>;
+  tags: Collection<Tag>;
+  authors: Collection<Author>;
+}
+
+const uri = process.env.CONTEXT_DB_URL!;
+
+class ContextSingleton {
+  private static instance: Context | null = null;
+
+  static async getInstance(): Promise<Context> {
+    if (!ContextSingleton.instance) {
+      const client = new MongoClient(uri);
+      await client.connect();
+      const database = client.db('mydatabase');
+      const books = database.collection<Book>('books');
+      const tags = database.collection<Tag>('tags');
+      const authors = database.collection<Author>('authors');
+      ContextSingleton.instance = { books, tags, authors };
+    }
+    return ContextSingleton.instance;
+  }
+}
+
+export async function createContext(): Promise<Context> {
+  return ContextSingleton.getInstance();
+}
+
+export async function createBook(book: Book): Promise<Book> {
+  const context = await ContextSingleton.getInstance();
+
+  const result = await context.books.insertOne(book);
+  return getBook(result.insertedId);
+}
+
+export async function getOrCreateBook(book: Book): Promise<Book> {
+  const context = await ContextSingleton.getInstance();
+  const existingBook = await context.books.findOne({
+    id: book.id,
+  });
+  if (existingBook) {
+    return existingBook;
+  }
+  return await createBook(book);
+}
+
+export async function getBook(id: ObjectId): Promise<Book | null> {
+  const context = await ContextSingleton.getInstance();
+  return context.books.findOne({ _id: id });
+}
+
+export async function updateBook(
+  id: ObjectId,
+  updates: Partial<Book>,
+): Promise<Book | null> {
+  const context = await ContextSingleton.getInstance();
+  const result = await context.books.findOneAndUpdate(
+    { _id: id },
+    { $set: updates },
+    { returnDocument: 'after' },
+  );
+  return result;
+}
+
+export async function deleteBook(id: ObjectId): Promise<boolean> {
+  const context = await ContextSingleton.getInstance();
+  const result = await context.books.deleteOne({ _id: id });
+  return result.deletedCount === 1;
+}
+export async function addAuthorToBook(
+  bookId: ObjectId,
+  author: Author,
+): Promise<Book | null> {
+  const context = await ContextSingleton.getInstance();
+  const result = await context.books.findOneAndUpdate(
+    { _id: bookId },
+    { $push: { authors: author } },
+    { returnDocument: 'after' },
+  );
+  return result;
+}
+
+export async function removeAuthorFromBook(
+  bookId: ObjectId,
+  authorId: ObjectId,
+): Promise<Book | null> {
+  const context = await ContextSingleton.getInstance();
+  const result = await context.books.findOneAndUpdate(
+    { _id: bookId },
+    { $pull: { 'authors._id': authorId } },
+    { returnDocument: 'after' },
+  );
+  return result;
+}
+
+export async function addTagToBook(
+  bookId: ObjectId,
+  tag: Tag,
+): Promise<Book | null> {
+  const context = await ContextSingleton.getInstance();
+  const result = await context.books.findOneAndUpdate(
+    { _id: bookId },
+    { $push: { tags: tag } },
+    { returnDocument: 'after' },
+  );
+  return result;
+}
+
+export async function removeTagFromBook(
+  bookId: ObjectId,
+  tagId: ObjectId,
+): Promise<Book | null> {
+  const context = await ContextSingleton.getInstance();
+  const result = await context.books.findOneAndUpdate(
+    { _id: bookId },
+    { $pull: { 'tags._id': tagId } },
+    { returnDocument: 'after' },
+  );
+  return result;
+}
+
+export async function reportError(
+  id: ObjectId,
+  error: string,
+  status?: Book['errors'][0],
+): Promise<void> {
+  const context = await ContextSingleton.getInstance();
+  await context.books.updateOne(
+    { _id: id },
+    { $push: { errors: error }, ...(status ? { status } : {}) },
+  );
+}
